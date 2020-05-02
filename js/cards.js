ddc2d3508ac13928c3f439b558c7ec4894d7a426
Nick Walters <nswalters.s@gmail.com>
2020-05-02 09:59:33 -0500

    Moved student-related code to its own js file

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,5 @@
 const buildStudentCard = (studentName, houseName) => {
 
-  // TODO: Sanitize studentName as it is user input!
-  // --sanitize--
-
-  // (houseName is generated only in this script, so no need to sanitize that)
-
   let card = `
   <div class="col-auto mb-3 px-0">
     <div class="card text-center">
@@ -27,20 +22,3 @@ const buildStudentCards = (studentsArray) => {
 
   printToDom('cards-row', domString);
 };
-
-const sortStudent = (studentName) => {
-
-  student = {
-    name: studentName,
-    isExpelled: false,
-    house: null,
-  }
-
-  students.push(student);
-
-  // TODO: generate a random house to assign to the student
-  // student.house = <random house name>
-
-  // re-build student cards
-  buildStudentCards(students);
-}
