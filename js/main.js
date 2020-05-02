5e32ff25bceadb28bb57fe5d63e13477015b67f3
Nick Walters <nswalters.s@gmail.com>
2020-05-02 09:40:59 -0500

    Moved cards-related functions to separate js file

--- js/main.js
+++ js/main.js
@@ -8,53 +8,6 @@ const printToDom = (selector, domString) => {
 
 }
 
-const buildStudentCard = (studentName, houseName) => {
-
-  // TODO: Sanitize studentName as it is user input!
-  // --sanitize--
-
-  // (houseName is generated only in this script, so no need to sanitize that)
-
-  let card = `
-  <div class="col-auto mb-3 px-0">
-    <div class="card text-center">
-      <h3 class="mt-3">${studentName}</h3>
-      <h6>${houseName}</h6>
-      <button class="btn btn-danger w-50 mx-auto my-3">Expel</button>
-    </div>
-  </div>
-  `
-  return card;
-}
-
-const buildStudentCards = (studentsArray) => {
-
-  domString = '';
-
-  students.forEach(student => {
-    domString += buildStudentCard(student.name, student.house);
-  })
-
-  printToDom('cards-row', domString);
-};
-
-const sortStudent = (studentName) => {
-
-  student = {
-    name: studentName,
-    isExpelled: false,
-    house: null,
-  }
-
-  students.push(student);
-
-  // TODO: generate a random house to assign to the student
-  // student.house = <random house name>
-
-  // re-build student cards
-  buildStudentCards(students);
-}
-
 const validateStudent = (event) => {
   // Prevent any default actions (such as submitting the form when Enter is pressed, which resets the page)
   event.preventDefault();
